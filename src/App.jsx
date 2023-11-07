10653f9c6a42b93e3e0d504b5947b0e81706cb90
wenderf5 <wenderfabianoalves@gmail.com>
2023-11-06 21:14:10 -0300

    1.1

--- src/App.jsx
+++ src/App.jsx
@@ -22,13 +22,13 @@ function App() {
         const appId = '641661';
         const appSecret = '775f1c363eee73b902c11b2fbc40133e';
         const accessToken = `${appId}:${appSecret}`;
-
-        //se for rodar o projeto localmente usar `https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${artistName}` `/api/search?q=${artistName}`  no fetch.
-        const searchEndpoint = `/api/search?q=${artistName}` ;
+        const searchEndpoint = `https://deezerdevs-deezer.p.rapidapi.com/search?q=${artistName}`;
 
         const requestOptions = {
             headers: {
                 'Authorization': `Bearer ${accessToken}`,
+                'X-RapidAPI-Key': '145cdbf4a7mshc71f9b1c1cc64dcp1b5e56jsn4134986c0b17',
+                'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
             },
         };
 
@@ -48,7 +48,7 @@ function App() {
     return (
         <div>
             <div className='navbar'>
-                <div className='titulo1'><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADQAAAA0CAYAAADFeBvrAAAAAXNSR0IArs4c6QAAAZRJREFUaEPtWG1ugzAMxQIicYqtJ9l6kq0nmXqSticZu0l3CqR8yJWltKo0DZMAASLzN06M37OfnUCR2QeZxVNIQGtnVBgShhIjICmXGPBgd8JQMGSJNwhDqQDXWn8CwInzZ63dNU1zvdutliEJyFMkDHE5PdV6dinXdd1rVVUfHEDW2ssmRIEL5L/10TVEqcE5d861zyhy9rRODJVl+c7ZKqXOzzajAjLGkMNvzmlRFPu6rtsBdg+TRWpIAgqgSBiaorFml3KkREOyKFTh7ioXc/YolRviMLXNqIB8r/jifhoALiTbvme9MfY/1FsonRGRnRScc8fJJoXQGjLGUM/qbZaIeFZKHTahchJQ6htrlimHiOy9HwAOXhTIlhs4W6ohLwrs2c65/WSiwKnbEuujZHuJH+Z8/glIa30CgN47DiJelVK7TdSQBBRwwVukDwlDa2fIF3rvAImIvzRAhj41+fnspU+pYs9e/ds2J8+zPWPFOp5rX/6NdS7kUp0rDKVCOtaPMBSLXKp9wlAqpGP9ZMfQDatQt0RwoWDYAAAAAElFTkSuQmCC"/><h1>Track Music</h1></div>
+                <div className='titulo1'><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADQAAAA0CAYAAADFeBvrAAAAAXNSR0IArs4c6QAAAZRJREFUaEPtWG1ugzAMxQIicYqtJ9l6kq0nmXqSticZu0l3CqR8yJWltKo0DZMAASLzN06M37OfnUCR2QeZxVNIQGtnVBgShhIjICmXGPBgd8JQMGSJNwhDqQDXWn8CwInzZ63dNU1zvdutliEJyFMkDHE5PdV6dinXdd1rVVUfHEDW2ssmRIEL5L/10TVEqcE5d861zyhy9rRODJVl+c7ZKqXOzzajAjLGkMNvzmlRFPu6rtsBdg+TRWpIAgqgSBiaorFml3KkREOyKFTh7ioXc/YolRviMLXNqIB8r/jifhoALiTbvme9MfY/1FsonRGRnRScc8fJJoXQGjLGUM/qbZaIeFZKHTahchJQ6htrlimHiOy9HwAOXhTIlhs4W6ohLwrs2c65/WSiwKnbEuujZHuJH+Z8/glIa30CgN47DiJelVK7TdSQBBRwwVukDwlDa2fIF3rvAImIvzRAhj41+fnspU+pYs9e/ds2J8+zPWPFOp5rX/6NdS7kUp0rDKVCOtaPMBSLXKp9wlAqpGP9ZMfQDatQt0RwoWDYAAAAAElFTkSuQmCC" /><h1>Track Music</h1></div>
             </div>
             <div className='blocoPrincipal'>
                 <Pesquisa handleChange={handleChange} CallApi={CallApi} />
@@ -60,10 +60,10 @@ function App() {
                 </div>
             </div>
             <div className='bloco2'>
-                <FtMusica currentTrack={currentTrack}/>
+                <FtMusica currentTrack={currentTrack} />
             </div>
-            <FtMusicamobile currentTrack={currentTrack}/>
-            
+            <FtMusicamobile currentTrack={currentTrack} />
+
         </div>
     )
 }
